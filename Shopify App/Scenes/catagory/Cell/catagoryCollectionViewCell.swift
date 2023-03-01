0dacf57df862965ab0f9e38f792d319a67d7737d
Ali Moustafa <alimoustafa7070@gmail.com>
2023-03-01 13:48:53 +0200

    make icon of tabBar and color and font

---
+++ Shopify App/Scenes/catagory/Cell/catagoryCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  catagoryCollectionViewCell.swift
+//  Shopify App
+//
+//  Created by Ali Moustafa on 01/03/2023.
+//
+
+import UIKit
+
+class catagoryCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Shopify App/Scenes/catagory/catagoryViewController.swift
@@ -0,0 +1,48 @@
+//
+//  catagoryViewController.swift
+//  Shopify App
+//
+//  Created by Ali Moustafa on 01/03/2023.
+//
+
+import UIKit
+
+class catagoryViewController: UIViewController {
+    
+    
+    @IBOutlet weak var product: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        product.reloadData()
+    }
+    
+
+    func  registerCollectionView(){
+        product.delegate = self
+        product.dataSource = self
+        
+    }
+
+}
+
+extension catagoryViewController: UICollectionViewDelegate, UICollectionViewDataSource{
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+}
+
+
+extension catagoryViewController:UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+    }
+}
