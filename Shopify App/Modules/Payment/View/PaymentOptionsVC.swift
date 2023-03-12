f97ddd538782d89e45139be359be6eeb6d67c297
Ali Moustafa <alimoustafa7070@gmail.com>
2023-03-12 18:03:08 +0200

    payment

--- Shopify App/Modules/Payment/View/PaymentOptionsVC.swift
+++ Shopify App/Modules/Payment/View/PaymentOptionsVC.swift
@@ -6,11 +6,25 @@
 //
 
 import UIKit
-
+import PassKit
 class PaymentOptionsVC: UIViewController {
     
     var selectedPaymentOption: String?
     var index: Int?
+    
+    
+    //MARK: - LOCAL PROPERTIES
+    private var paymentRequest : PKPaymentRequest = {
+        let request = PKPaymentRequest ()
+        request.merchantIdentifier = "merchant.com.pushpendra.pay"
+        request.supportedNetworks = [.quicPay, .masterCard, .visa]
+        request.supportedCountries = ["EG", "US" ]
+        request.merchantCapabilities = .capability3DS
+        request.countryCode = "EG"
+        request.currencyCode = "EGP"
+        request.paymentSummaryItems = [PKPaymentSummaryItem(label: "shopify", amount: 123000)]
+        return request}()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -56,30 +70,55 @@ extension PaymentOptionsVC: UITableViewDelegate, UITableViewDataSource{
         }
     }
 
-    
+ 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PaymentOptionCell", for: indexPath) as! PaymentOptionsCell
         
         if indexPath.section == 0 {
-            cell.checkPaymentWay.setImage(UIImage(systemName: selectedPaymentOption == "Apple Pay" ? "circle" : "circle.fill"), for: .normal)
+            let image = selectedPaymentOption == "Apple Pay" ? UIImage(systemName: "circle.fill") : UIImage(systemName: "circle")
+            cell.checkPaymentWay.setImage(image, for: .normal)
             cell.PaymentWayName.text = "Apple Pay"
         } else {
-            cell.checkPaymentWay.setImage(UIImage(systemName: selectedPaymentOption == "Cash On Delivery (COD)" ? "circle" : "circle.fill"), for: .normal)
+            let image = selectedPaymentOption == "Cash On Delivery (COD)" ? UIImage(systemName: "circle.fill") : UIImage(systemName: "circle")
+            cell.checkPaymentWay.setImage(image, for: .normal)
             cell.PaymentWayName.text = "Cash On Delivery (COD)"
         }
         
         return cell
     }
+
+    
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.section == 0 {
-            selectedPaymentOption = "Cash On Delivery (COD)"
-        } else {
+            tapForPay ()
             selectedPaymentOption = "Apple Pay"
+        } else {
+            selectedPaymentOption = "Cash On Delivery (COD)"
         }
         tableView.reloadData()
     }
 
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 80
+    }
+}
+
+extension PaymentOptionsVC : PKPaymentAuthorizationViewControllerDelegate {
+    func paymentAuthorizationViewControllerDidFinish(_ controller: PKPaymentAuthorizationViewController) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+    func paymentAuthorizationViewController(_ controller: PKPaymentAuthorizationViewController, didAuthorizePayment
+                                            payment: PKPayment, handler completion: @escaping (PKPaymentAuthorizationResult) -> Void) {
+        completion(PKPaymentAuthorizationResult(status: .success, errors: nil))
+    }
+    
+    @objc func tapForPay (){
+        let controller = PKPaymentAuthorizationViewController(paymentRequest: paymentRequest)
+        if controller != nil{
+            controller!.delegate = self
+            present (controller!, animated: true)
+        }
+       
     }
 }
